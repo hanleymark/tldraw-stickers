c79d7f562e14a699836c4418da9f743992253f2e
Mark hanley <home@markhanley.co.uk>
2023-03-31 14:52:26 +0100

    Set up start and stop drag event handlers

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 import { StampButton } from "./modules/stamp-button.js";
 
-const toolbarContainer = document.querySelector('#toolbar__container');
+const toolbarContainer = document.querySelector("#toolbar__container");
+const canvas = document.querySelector("#canvas");
 
-const stampItems = [
-    "🌟",
-    "🔥",
-    "💖",
-    "👍",
-    "👎",
-];
+const stampItems = ["🌟", "🔥", "💖", "👍", "👎"];
 
 setUpStampButtons(stampItems);
 
 function setUpStampButtons(stampItems) {
-    const stampButtons = stampItems.map((stampItem, index) => {
-        return new StampButton(`stamp-button-${index}`, stampItem, "toolbar__button");
-    });
-    
-    stampButtons.forEach((button) => {
-        toolbarContainer.appendChild(button.getElement());
-    });
-}
+  const stampButtons = stampItems.map((stampItem, index) => {
+    return new StampButton(
+      `stamp-button-${index}`,
+      stampItem,
+      "toolbar__button",
+      canvas
+    );
+  });
 
+  stampButtons.forEach((button) => {
+    toolbarContainer.appendChild(button.getButtonElement());
+  });
+}
--- modules/stamp-button.js
+++ modules/stamp-button.js
@@ -1,18 +1,52 @@
 export class StampButton {
-  constructor(id, stamp, style) {
+  constructor(id, stamp, style, canvas) {
     this.id = id;
     this.stamp = stamp;
     this.style = style;
+    this.canvas = canvas;
     this.element = document.createElement("button");
   }
-  getElement = () => {
+
+  getButtonElement = () => {
     this.element.id = this.id;
     this.element.classList.add(this.style);
     this.element.innerHTML = this.stamp;
-    this.element.addEventListener("click", this.handleClick);
+    this.element.addEventListener("mousedown", (event) => this.handleClick(event));
     return this.element;
+  };
+
+  getStampElement = () => {
+    const stampElement = document.createElement("div");
+    stampElement.classList.add("stamp__element--moving");
+    stampElement.innerHTML = this.stamp;
+    window.addEventListener("mousemove", (event) => this.startDrag(event));
+    window.addEventListener("mouseup", (event) => this.stopDrag(event));
+    return stampElement;
   }
-    handleClick = () => {
-      console.log(`Stamp button ${this.id} clicked!`);
+
+  handleClick = (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const stampElement = this.getStampElement();
+    stampElement.style.left = `${mouseX}px`;
+    stampElement.style.top = `${mouseY}px`;
+    this.canvas.appendChild(stampElement);
+  };
+
+  startDrag = (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const stampElement = this.canvas.querySelector(".stamp__element--moving");
+    if (stampElement) {
+      stampElement.style.left = `${mouseX}px`;
+      stampElement.style.top = `${mouseY}px`;
     }
+  }
+
+  stopDrag = (event) => {
+    const stampElement = this.canvas.querySelector(".stamp__element--moving");
+    if (stampElement) {
+      stampElement.classList.remove("stamp__element--moving");
+    }
+  }
 }
